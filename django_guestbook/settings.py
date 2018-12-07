a5f1aba749196da816324daf9fb1f6e18f06d4c3
basecamp <cvanderpoel@basecampcodingacademy.org>
2018-12-07 11:01:37 -0600

    I connected a database to it

--- django_guestbook/settings.py
+++ django_guestbook/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,8 @@ WSGI_APPLICATION = 'django_guestbook.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_guest-book',
     }
 }
 
