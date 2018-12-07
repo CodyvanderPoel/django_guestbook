de0842883a4ca108328e60ac42642cbb36890e80
basecamp <cvanderpoel@basecampcodingacademy.org>
2018-12-07 11:01:03 -0600

    I setup my views and I am working on the Note Post

--- app/views.py
+++ app/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views import View
+from app import forms
 
 # Create your views here.
+
+
+class Home(View):
+    def get(self, request):
+        return render(request, 'home.html')
+
+
+class Note(View):
+    def get(self, request):
+        return render(request, 'note.html', {'NoteForm': forms.NoteForm()})
+
+    def post(self, request):
+        form = forms.NoteForm(data=request.POST)
+        if form.is_valid():
+            name, date, note = form.cleaned_data['name'], form.cleaned_data[
+                'date'], form.cleaned_data['note']
+            return render(request, 'note.html')
